e195e10f504af167a699111352e884ac6f47e931
Rharhuky <rharhuky@gmail.com>
2025-03-04 00:15:38 -0300

    test (Service): findById operation at UserService

--- backend/instagram/src/test/java/br/edu/ifpb/instagram/service/impl/UserServiceImplTest.java
+++ backend/instagram/src/test/java/br/edu/ifpb/instagram/service/impl/UserServiceImplTest.java
@@ -1,72 +1,61 @@
 package br.edu.ifpb.instagram.service.impl;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.util.Optional;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
 import br.edu.ifpb.instagram.model.dto.UserDto;
 import br.edu.ifpb.instagram.model.entity.UserEntity;
 import br.edu.ifpb.instagram.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootTest
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_METHOD;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(PER_METHOD)
 public class UserServiceImplTest {
 
-    @MockitoBean
-    UserRepository userRepository; // Repositório simulado
+    @Mock
+    private UserRepository userRepository;
 
-    @Autowired
-    UserServiceImpl userService; // Classe sob teste
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserServiceImpl userService;
 
     @Test
-    void testFindById_ReturnsUserDto() {
-        // Configurar o comportamento do mock
-        Long userId = 1L;
+    void findById_WithNoExistingUserEntity_ThrowsRuntimeException() {
+        var id = 1L;
+        var sut = catchThrowable(() -> userService.findById(id));
+        assertThat(sut)
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("User not found");
 
-        UserEntity mockUserEntity = new UserEntity();
-        mockUserEntity.setId(userId);
-        mockUserEntity.setFullName("Paulo Pereira");
-        mockUserEntity.setEmail("paulo@ppereira.dev");
-
-        when(userRepository.findById(userId)).thenReturn(Optional.of(mockUserEntity));
-
-        // Executar o método a ser testado
-        UserDto userDto = userService.findById(userId);
-
-        // Verificar o resultado
-        assertNotNull(userDto);
-        assertEquals(mockUserEntity.getId(), userDto.id());
-        assertEquals(mockUserEntity.getFullName(), userDto.fullName());
-        assertEquals(mockUserEntity.getEmail(), userDto.email());
-
-        // Verificar a interação com o mock
-        verify(userRepository, times(1)).findById(userId);
     }
 
     @Test
-    void testFindById_ThrowsExceptionWhenUserNotFound() {
-        // Configurar o comportamento do mock
-        Long userId = 999L;
+    void findById_WithExistingUser_ReturnsUserEntity() {
+        var id = 1L;
+        var userEntity = new UserEntity();
+        userEntity.setId(id);
+        userEntity.setEmail("userEntity@mail.com");
+        userEntity.setFullName("The user entity name");
+        userEntity.setEncryptedPassword("userPass");
+        ArgumentCaptor<Long> userId = ArgumentCaptor.forClass(Long.class);
+        when(userRepository.findById(userId.capture())).thenReturn(Optional.of(userEntity));
 
-        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+        var sut = userService.findById(id);
 
-        // Executar e verificar a exceção
-        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
-            userService.findById(userId);
-        });
-
-        assertEquals("User not found", exception.getMessage());
-
-        // Verificar a interação com o mock
-        verify(userRepository, times(1)).findById(userId);
+        assertNotNull(sut);
+        assertInstanceOf(UserDto.class, sut,"Must be instance of UserDto");
+        assertEquals(userEntity.getEmail(), sut.email());
+        assertEquals(id, userId.getValue());
     }
 }
